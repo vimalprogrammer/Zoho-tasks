d686820dc1a156ef39450ee51c60dfece537d258
vimalprogrammer <msvimal10@gmail.com>
2022-07-17 08:27:45 +0530

    Display Parking class added that takes CarParker obj in constructor parameter

---
+++ DisplayParking.java
@@ -0,0 +1,32 @@
+public class DisplayParking
+{
+    private ParkSystem CarParker;
+    public DisplayParking(ParkSystem CarParker)
+    {
+        this.CarParker=CarParker;
+    }
+//   public void displayStatus() 
+//   {
+//     System.out.println();
+//     System.out.println("Slot No\t| Car");
+    
+//     for (int i = 0; i < super.totalNumberOfSlots; i++) 
+//     {
+//       if (super.occupiedSlots[i] != null) 
+//         System.out.println(i + 1 + "\t| " + occupiedSlots[i].getRegNo());
+//     }
+//     System.out.println();
+//   }
+  
+  
+  public void displayStatus(){
+     Car arr[]= CarParker.getOccupiedSlots();
+     int count=0;
+     System.out.println("Slot No\t| Car");
+     for(Car i: arr){
+         if (i != null) 
+             System.out.println(count + 1 + "\t| " + i.getRegNo());
+        count++;
+     }
+  }
+}
