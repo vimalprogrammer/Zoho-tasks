154b95300256c3b827059bc5b9bb3e4f967e3818
vimalprogrammer <msvimal10@gmail.com>
2022-07-27 22:37:34 +0530

    servlet & jsp - servlet to servlet redirect

---
+++ Servlet&JSP/Demo1/WEB-INF/classes/Servlet2servlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/v")
+public class Servlet2servlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		
+		int sol=(int)req.getAttribute("r");//it is object so converting into string
+		PrintWriter out=res.getWriter();
+
+		out.println("Result(Servlet 2 Servlet) : "+sol);
+
+	}
+}
--- Servlet&JSP/Demo1/WEB-INF/classes/ServletDemo.java
+++ Servlet&JSP/Demo1/WEB-INF/classes/ServletDemo.java
@@ -1,26 +1,36 @@
 
-
-import javax.servlet.http.HttpServletRequest;
-
+import java.io.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
 
-
-public class ServletDemo  extends HttpServlet
+@WebServlet("/add")
+public class ServletDemo extends HttpServlet 
 {
-	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+	public void doGet(HttpServletRequest req,HttpServletResponse res) 
 	{
-		int n1=Integer.parseInt(req.getParameter("num1"));
-		int n2=Integer.parseInt(req.getParameter("num2"));
-		
-		int result=n1+n2;
-		
-		PrintWriter out=res.getWriter();
-		
-		out.println("Result : "+result);
+		try
+		{
+			int n1=Integer.parseInt(req.getParameter("num1"));
+			int n2=Integer.parseInt(req.getParameter("num2"));
+			
+			int result=n1+n2;
+
+			req.setAttribute("r",result);
+
+			RequestDispatcher rd=req.getRequestDispatcher("v"); 
+			rd.forward(req,res);
+			// PrintWriter out=res.getWriter();
+			// out.print("result " + result);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 		
 	}
 }
