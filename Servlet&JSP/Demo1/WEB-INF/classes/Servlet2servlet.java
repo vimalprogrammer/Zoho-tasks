5c81ae6788b335e99349a9d4f430dfd14508784c
vimalprogrammer <msvimal10@gmail.com>
2022-07-28 10:26:50 +0530

    servlet & jsp - Cookies and Session

--- Servlet&JSP/Demo1/WEB-INF/classes/Servlet2servlet.java
+++ Servlet&JSP/Demo1/WEB-INF/classes/Servlet2servlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Cookie;
 
 @WebServlet("/v")
 public class Servlet2servlet extends HttpServlet
@@ -17,10 +19,27 @@ public class Servlet2servlet extends HttpServlet
 		
 		// int sol=(int)req.getAttribute("r");//it is object so converting into string
 
-		int sol=Integer.parseInt(req.getParameter("result"));
-		sol=sol*sol;
-		PrintWriter out=res.getWriter();
-		out.println("Result(Servlet 2 Servlet) : "+ sol);
+		// int sol=Integer.parseInt(req.getParameter("result"));
+		// sol=sol*sol;
+		//PrintWriter out=res.getWriter();
 
-	}
+		//HttpSession session = req.getSession();
+
+		int sol=1;
+		Cookie cookies[]=req.getCookies();
+		for(Cookie c:cookies)
+		{
+			if(c.getName().equals("r"))
+				sol=Integer.parseInt(c.getValue());
+		}
+
+	 	//int sol=(int) session.getAttribute("r");
+
+	 	sol=sol*sol;
+		//out.println("Result(Servlet 2 Servlet) : "+ sol);
+		PrintWriter out=res.getWriter();
+		out.println("Result(Servlet 2 Servlet) Using Session : "+ sol);
+
+
+	}   
 }
--- Servlet&JSP/Demo1/WEB-INF/classes/ServletDemo.java
+++ Servlet&JSP/Demo1/WEB-INF/classes/ServletDemo.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Cookie;
+
 
 @WebServlet("/add")
 public class ServletDemo extends HttpServlet 
@@ -20,16 +24,24 @@ public class ServletDemo extends HttpServlet
 			
 			int result=n1+n2;
 
-			res.sendRedirect("v?result="+result);//URL Rewriting
-			// req.setAttribute("r",result);
+			// PrintWriter out=res.getWriter();
+			// out.print("result " + result);
 
 			// RequestDispatcher rd=req.getRequestDispatcher("v"); 
 			// rd.forward(req,res);
 
+			//res.sendRedirect("v?result="+result);//URL Rewriting
+			// req.setAttribute("r",result);
 
+			//HttpSession session = req.getSession();
+			//session.setAttribute("r",result);//key, value
 
-			// PrintWriter out=res.getWriter();
-			// out.print("result " + result);
+			
+			Cookie cookie = new Cookie("r",result+"");
+			res.addCookie(cookie);
+
+			res.sendRedirect("v");
+
 		}
 		catch(Exception e)
 		{
